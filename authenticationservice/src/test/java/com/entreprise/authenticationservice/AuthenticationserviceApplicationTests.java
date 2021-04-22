ad27d340bfde7c575bfe076a0a4c582d75c96453
dmalembe <malembedidier24@gmail.com>
2021-04-22 13:44:13 +0200

    Init project

---
+++ authenticationservice/src/test/java/com/entreprise/authenticationservice/AuthenticationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entreprise.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
