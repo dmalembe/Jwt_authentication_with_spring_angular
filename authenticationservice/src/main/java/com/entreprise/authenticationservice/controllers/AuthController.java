2b8ce7f514ffdbc6e7083f2625341ab55d13a45c
dmalembe <malembedidier24@gmail.com>
2021-04-25 01:45:55 +0200

    Maj Backend + Creation services ng

--- authenticationservice/src/main/java/com/entreprise/authenticationservice/controllers/AuthController.java
+++ authenticationservice/src/main/java/com/entreprise/authenticationservice/controllers/AuthController.java
@@ -29,6 +29,13 @@ import com.entreprise.authenticationservice.payload.response.MessagesResponse;
 import com.entreprise.authenticationservice.security.jwt.JwtUtils;
 import com.entreprise.authenticationservice.security.services.UserDetailsImpl;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
 	@Autowired
