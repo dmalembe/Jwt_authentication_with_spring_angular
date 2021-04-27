316c479a788cb8d51e14223157bcf6fe71c5b755
dmalembe <malembedidier24@gmail.com>
2021-04-27 11:19:24 +0200

    Maj project

--- authenticationservice-UI/src/app/profile/profile.component.ts
+++ authenticationservice-UI/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../service/token-storage.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  currentUser: any;
+  
+  constructor(private token: TokenStorageService) { }
 
   ngOnInit(): void {
+    this.currentUser = this.token.getUser();
   }
 
 }
